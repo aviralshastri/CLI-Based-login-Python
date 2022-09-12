edcb697c765fe0b5f6f0d1d92c40ca7d324da309
aviralshastri <104085688+aviralshastri@users.noreply.github.com>
2022-09-12 20:25:17 +0530

    Add files via upload

---
+++ login page.py
@@ -0,0 +1,80 @@
+import smtplib
+import random
+
+def otpgenrator(x):
+    otp=''.join([str(random.randint(0,9))for i in range (0,4)])
+    server=smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login('otpbot01@gmail.com','bpcuzmrwrboatjdl')
+    server.sendmail('otpbot01',x,otp)
+    return otp
+
+def login():
+     l=input("username:")
+     m=input("password:")
+     elementn = l+':'+m+'\n'
+     element = l+':'+m
+     file = open("idpassword.txt", "r+")
+     y = file.readlines()
+     g=0
+     while True:
+         if elementn in y:
+             print("Username and password checked")
+             otp = otpgenrator(l)
+             while True:
+                 k = str(input("otp: "))
+                 if k == otp:
+                     print("Login succesfull!!")
+                     break
+                 elif k !=otp:
+                     print("Wrong otp")
+                     print("Re-enter the otp")
+             break
+         elif element in y:
+             print("Username and password checked")
+             otp = otpgenrator(l)
+             while True:
+                 k = str(input("otp: "))
+                 if k == otp:
+                     print("Login succesfull!!")
+                     break
+                 else:
+                     print("Wrong otp")
+                     print("Re-enter the otp")
+             break
+
+         else:
+             g=g+1
+             print("invalid username or password")
+             break
+     file.close()
+     return l
+
+q=input("Do you have your account registered (yes/no): ")
+q=q.lower()
+
+if q=='yes':
+    login()
+
+elif q=='no':
+    while True:
+        p=input("Wanna create the account (yes/no): ")
+        p=p.lower()
+        if p == 'yes':
+            l = input("Enter your gmail: ")
+            n = input("Create password: ")
+            file = open("idpassword.txt", 'r+')
+            p = l + ":" + n + "\n"
+            if p in file:
+                print("This account is already registered")
+            else:
+                file.writelines(p)
+                file.close()
+                print("(: Account created successfully. Now you can Login to the account :)")
+            login()
+            break
+        elif p == 'no':
+            login()
+            break
+        else:
+            print("Invalid input")
